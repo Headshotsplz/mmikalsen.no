6ea8c7215f2b48df746ee26d03f17ca4d9b8b98f
Headshotsplz <markusandresen11@gmail.com>
2026-09-18 22:14:15 +0200

    Ta med statistikk fra OSI 2 i 22/23

--- api/stats.js
+++ api/stats.js
@@ -5,7 +5,7 @@
 const BASE = 'https://nvbf-web.dataproject.com';
 const PLAYER_ID = '803';
 // Bare statistikk fra disse lagene vises på siden.
-const TEAMS = ['NTNUI 2'];
+const TEAMS = ['OSI 2', 'NTNUI 2'];
 
 async function fetchWithTimeout(url, options = {}) {
   return fetch(url, { ...options, signal: AbortSignal.timeout(8000) });
